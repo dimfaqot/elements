69634f6a8a493eb271f48ef73cd17b54f31bdb63
Dim Faqot <dimyfaqot@gmail.com>
2022-09-16 21:52:12 +0700

    Revisi kedua

--- login.php
+++ login.php
@@ -1,47 +1,63 @@
-public function auth()
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
 {
-helper('routes');
-$username = $this->request->getVar('username');
-$password = $this->request->getVar('password');
+    public function index()
+    {
+        return view('templates/guest');
+    }
 
-if ($username == "" || $password == "") {
-session()->setFlashdata('gagal', "Gagal!. Username dan password harus diisi!");
-return redirect()->to(base_url('login'));
-}
+    public function auth()
+    {
+        helper('routes');
+        $username = $this->request->getVar('username');
+        $password = $this->request->getVar('password');
 
-$user = \App\Models\Users::class;
-$user = new $user;
+        if ($username == "" || $password == "") {
+            session()->setFlashdata('gagal', "Gagal!. Username dan password harus diisi!");
+            return redirect()->to(base_url('login'));
+        }
 
-// cek password sekarang
-$q = $user->where('username', $username)->first();
-if (!$q) {
-session()->setFlashdata('gagal', "Gagal!. Username tidak ditemukan!");
-return redirect()->to(base_url('login'));
-} else {
-if (!password_verify($password, $q['password'])) {
-session()->setFlashdata('gagal', "Gagal!. Password salah!");
-return redirect()->to(base_url('login'));
-} else {
-session()->set([
-'id' => $q['id'],
-'username' => $q['username'],
-'nama' => $q['nama'],
-'role_id' => $q['role_id']
-]);
+        $user = \App\Models\Users::class;
+        $user = new $user;
 
-return redirect()->to(base_url("dashboard"));
-}
-}
-}
+        // cek password sekarang
+        $q = $user->where('username', $username)->first();
+        if (!$q) {
+            session()->setFlashdata('gagal', "Gagal!. Username tidak ditemukan!");
+            return redirect()->to(base_url('login'));
+        } else {
+            if (!password_verify($password, $q['password'])) {
+                session()->setFlashdata('gagal', "Gagal!. Password salah!");
+                return redirect()->to(base_url('login'));
+            } else {
+                session()->set([
+                    'id' => $q['id'],
+                    'username' => $q['username'],
+                    'alias' => $q['alias'],
+                    'image' => $q['image'],
+                    'nama' => $q['nama'],
+                    'role' => $q['role']
+                ]);
+
+                return redirect()->to(base_url("dashboard"));
+            }
+        }
+    }
 
 
-public function logout()
-{
-helper('routes');
-session()->remove('id');
-session()->remove('username');
-session()->remove('nama');
-session()->remove('role_id');
+    public function logout()
+    {
+        helper('routes');
+        session()->remove('id');
+        session()->remove('username');
+        session()->remove('nama');
+        session()->remove('role');
+        session()->remove('alias');
+        session()->remove('image');
 
-return redirect()->to(base_url("login"));
+        return redirect()->to(base_url());
+    }
 }
